292b4daa429a0ab67d4c4e30ab00966b6e5be898
Kuznetzz <36985722+Kuznetzz@users.noreply.github.com>
2019-07-24 16:55:53 +0800

    Update aviasales.py

--- aviasales.py
+++ aviasales.py
@@ -1,51 +1,53 @@
-# Задание 2. В приложении парсинга википедии получить первую ссылку на
-# другую страницу и вывести все значимые слова из неё. Результат записать
-# в файл в форматированном виде
+# Задание 1. Доработать приложение по поиску авиабилетов, чтобы
+# оно возвращало билеты по названию города, а не по IATA коду.
+# Пункт отправления и пункт назначения должны передаваться в качестве
+# параметров. Сделать форматированный вывод, который содержит в себе
+# пункт отправления, пункт назначения, дату вылета, цену билета
+# (можно добавить еще другие параметры по желанию)
 
-# Задание 3.*Научить приложение определять количество ссылок в статье.
-# Спарсить каждую ссылку и результаты записать в отдельные файлы.
-
-import yaml
-import collections
 import requests
-import re
-from urllib.parse import unquote
+import requests
+import json
+from argparse import ArgumentParser
 
-def return_wiki_html(topic):
-    wiki_request = requests.get(f'{url}{topic}')
-    return wiki_request.text
+# parsing arguments
 
-def return_words(topic):
-    wiki_html = return_wiki_html(topic)
-    words = re.findall('[а-яА-Я]{4,}', wiki_html)
-    words_counter = collections.Counter()
-    for word in words:
-        words_counter[word] += 1
-    return words_counter.most_common(10)
+parser = ArgumentParser()
 
-def parse_to_file(link):
-    count_words = return_words(link)
+parser.add_argument(
+    '-o', '--origin', type=str,
+    required=False, help='Origin name', default='MOW')
+parser.add_argument(
+    '-d', '--destination', type=str,
+    required=True, help='Destination name')
+args = parser.parse_args()
 
-    dict_for_yaml = {}
-    for i in count_words:
-        dict_for_yaml[i[0]] = i[1]
+flight_params = {
+    'origin': args.origin,
+    'destination': args.destination,
+    'one_way': 'true'}
 
-    with open(f'{link}.yaml', 'w', encoding='utf-8') as to_write:
-        yaml.dump(dict_for_yaml, to_write, default_flow_style=False, allow_unicode=True, sort_keys=False)
+# iata detection
+
+def iata_recon(data):
+    key = 'f2144820-33f5-4e27-befc-c6254065d2a0'
+    r = requests.get(f'https://airlabs.co/api/v6/cities?&code={data}&api_key={key}')
+    try:
+        return(r.json()['response'][0]['name'])
+    except:
+        print(f'Something wrong with {data}')
 
 
-url = 'https://ru.wikipedia.org/wiki/'
-topic = "Трамвай"
+origin = iata_recon(args.origin)
+destination = iata_recon(args.destination)
 
+# requesting
+req = requests.get("http://min-prices.aviasales.ru/calendar_preload", params=flight_params)
+data = req.json()['best_prices']
+data.sort(key=lambda k : k['depart_date'])
 
-print(f'Parsing {url+topic}')
-page = return_wiki_html(topic)
-links = [i for i in  re.findall(r'href=\"/wiki/(.*?)\"', page) if ":" not in i ][1:]
-print(f'Total {len(links)} links')
-print(f'Parsing and saving results for fist 10 links :')
-for i in links[:10]:
-    print(f'Parsing {url+unquote(i)}')
-    parse_to_file(unquote(i))
-    print(f'parsing saved in {unquote(i)}.yaml')
-print()
-print(f'Process Done ')
+keys = ['origin','destination','depart_date','value']
+data_filtered = [dict(zip(keys, [i[k] for k in keys])) for i in data]
+
+for i in data_filtered[:10]:
+    print(f'Flight from {origin} to {destination} at {i["depart_date"]} cost {i["value"]}')
